5387645ee85379e460701457a8477d9f8824bffc
my pc <my pc@LAPTOP-PH5VGOJ2>
2021-06-24 19:48:55 +0530

    My first project

---
+++ HelloWord/src/Student.java
@@ -0,0 +1,14 @@
+
+public class Student {
+	public static Student() {
+double name;
+int number;
+	public static void main(String[] args) {
+	Student obj1=new Student();
+	obj1.name(Bharath);
+	Student obj2=new Student();
+	obj2.number(1);
+	
+	}
+	}
+}
